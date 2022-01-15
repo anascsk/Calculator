910e511ba06f3cef1e9ffc103b0dde11e9f36d2a
anascsk <84561602+anascsk@users.noreply.github.com>
2022-01-15 21:53:47 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -37,36 +37,40 @@ function App() {
   };
 
   const deleteLast = () => {
-    if (calc == '') {
+    if (calc == "") {
       return;
     }
 
     const value = calc.slice(0, -1);
     setCalc(value);
-  }
+  };
 
-return (
-  <div className="App">
-    <div className="calculator">
-      <div className="display">
-        {result ? <span>({result})</span> : ""} {calc || "0"}
-      </div>
-      <div className="operators">
-        <button onClick={() => updateCalc("/")}>/</button>
-        <button onClick={() => updateCalc("*")}>*</button>
-        <button onClick={() => updateCalc("+")}>+</button>
-        <button onClick={() => updateCalc("-")}>-</button>
-        <button onClick={deleteLast}>DEL</button>
-      </div>
+  return (
+    <>
+    <div className="title">Simply Calc</div>
+    <div className="App">
+      
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>({result})</span> : ""} {calc || "0"}
+        </div>
+        <div className="operators">
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={deleteLast}>DEL</button>
+        </div>
 
-      <div className="digits">
-        {createDigits()}
-        <button onClick={() => updateCalc("0")}>0</button>
-        <button onClick={() => updateCalc(".")}>.</button>
-        <button onClick={calculate}>=</button>
+        <div className="digits">
+          {createDigits()}
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
       </div>
     </div>
-  </div>
-);
+    </>
+  );
 }
 export default App;
